d08fe62c7b6a57cc7c58b63fabeebe294bcbd7ae
Khalturin Evgenii <1tigrin@mail.ru>
2025-09-11 12:05:55 +0300

    Initial commit

---
+++ HorrorOfKI23-10b-1.cpp
@@ -0,0 +1,12 @@
+﻿// HorrorOfKI23-10b-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+
+
+}
